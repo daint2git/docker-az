b30a4b78e3a01ef2f26873624748777df169f82a
daint-1849 <nguyen.tran.dai@sun-asterisk.com>
2020-09-24 18:23:33 +0700

    update: node redis example

--- node-redis/src/app.mjs
+++ node-redis/src/app.mjs
@@ -1,23 +1,23 @@
-import express from 'express'
-import redis from 'redis'
+import express from "express";
+import redis from "redis";
 
 const app = express();
 const redisClient = redis.createClient({
   host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT
-})
+  port: process.env.REDIS_PORT,
+});
 const HOST = process.env.SERVER_HOST;
 const PORT = process.env.SERVER_PORT;
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-redisClient.on('error', console.error)
+redisClient.on("error", console.error);
 
-redisClient.set('xxx', 'value of xxx', redis.print)
-redisClient.get('xxx', redis.print)
+redisClient.set("xxx", "value of xxx", redis.print);
+redisClient.get("xxx", redis.print);
 
-app.listen(PORT, function() {
-  console.log(`Running on http://${HOST}:${PORT}`)
+app.listen(PORT, function () {
+  console.log(`Running on http://${HOST}:${PORT}`);
 });
